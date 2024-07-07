1fe89f0b3206ba50244027e47946b175ed0ffc0c
bcmes <brunomeloesilva@gmail.com>
2024-07-07 18:02:29 -0400

    SimpleAnnotation

--- src/main/java/com/bcms/apache/annotation/SimpleAnnotation.java
+++ src/main/java/com/bcms/apache/annotation/SimpleAnnotation.java
@@ -7,17 +7,23 @@ import java.lang.annotation.Target;
 
 @Target(ElementType.FIELD)
 @Retention(RetentionPolicy.RUNTIME)
-public @interface MyAnnotation {
-    String nomeCampo() default "Valor default";
+public @interface SimpleAnnotation {
+    String attributeName() default "default value x";
 }
-//Utilizaćão: @MyAnnotation(nomeCampo="Meu valor passado")
-//Se o atributo(metodo) for nomeado de value, nem precisa informar o nome do atributo, ex.:
-//@MyAnnotation("Meu valor passado")
+
+//Se o attributeName for renomeado de value, nem precisa informar o nome do atributo, ex.: @MyAnnotation("Meu valor passado")
 
 /**
  * Para definir parametros devemos seguir as seguintes regras:
  * - o tipo de retorno do método está restrito aos tipos primitivos, String, Class, Enum, Annotation ou arrays desses tipos.
- * - o nome do metodo será o nome do parametro Annotation
+ * - o nome do metodo será o nome do parametro na Annotation
  * * o metodo nao pode receber parametros e nem lancar excecoes
  * - o método pode ter um valor default associado, para caso do parametro nao ter valor setado
  */
+
+//Exemplo de utilização
+record ExampleObject(
+        @SimpleAnnotation(attributeName = "Pedro")
+        String name,
+        Integer age
+) { }
